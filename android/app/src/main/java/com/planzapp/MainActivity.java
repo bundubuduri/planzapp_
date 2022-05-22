57e7cde26a38cddb968a6fdbd6269523ec220a67
committ <66704935+committ@users.noreply.github.com>
2022-05-22 14:25:57 -0400

    Android Migration

--- android/app/src/main/java/com/planzapp/MainActivity.java
+++ android/app/src/main/java/com/planzapp/MainActivity.java
@@ -1,6 +1,20 @@
 package com.planzapp;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
 
 public class MainActivity extends FlutterActivity {
+     @Override
+     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+         GeneratedPluginRegistrant.registerWith(flutterEngine);
+         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                 .setMethodCallHandler(
+                     (call, result) -> {
+                         // Your existing code
+                 }
+         );
+     }
 }
